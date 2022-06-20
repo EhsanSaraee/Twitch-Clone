f402dd6134c0631f29df507426d7c1ce7f8d20c8
ehsan <ehsansaraee6570@yahoo.com>
2022-06-20 10:56:40 +0430

    implementing next-auth configuration with github account

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,13 @@
 import Navbar from '@/components/Navbar';
 import '../styles/globals.css';
+import { SessionProvider } from 'next-auth/react';
 
-const MyApp = ({ Component, pageProps }) => {
+const MyApp = ({ Component, pageProps, session }) => {
    return (
-      <>
+      <SessionProvider session={session}>
          <Navbar />
          <Component {...pageProps} />
-      </>
+      </SessionProvider>
    );
 };
 
